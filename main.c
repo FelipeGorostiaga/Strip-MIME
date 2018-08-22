8c90c288510c3c4dfd5b93f0b19b3562ce437187
tferrerITBA <tferrer@itba.edu.ar>
2018-08-22 20:48:30 -0300

    Parent receives child's output, dependant on parent's input

--- main.c
+++ main.c
@@ -8,13 +8,15 @@
 int main(int argc, char * argv []) {
     char * command = NULL;
     char commandOutput[1000] = {0};
-    char stdInput[BLOCK];
+    char stdInput[BLOCK] = {0};
     size_t cmdLength = 0;
     int i, j;
-    int inputFds[2] = {-1,-1};
-    int outputFds[2] = {-1,-1};
+    int fds[2] = {-1,-1};
     ssize_t bytesRead;
     int status;
+
+    if(argc <= 1)
+        return 0;
 
     for(i = 1; i < argc; i++) {
         for(j = 0; argv[i][j] != 0; j++) {
@@ -24,39 +26,29 @@ int main(int argc, char * argv []) {
             command[cmdLength++] = argv[i][j];
         }
     }
-    if(cmdLength % BLOCK == 0 ) {
+    if(cmdLength % BLOCK == 0) {
         command = realloc(command,cmdLength + BLOCK);
     }
 
-    if(pipe(inputFds) < 0) { exit(1); }
-    if(pipe(outputFds) < 0) { exit(1); }
-
     if(command != NULL) {
         command[cmdLength] = 0;
-        printf("\n");
     } else {
         exit(1);
     }
 
-    scanf("%s",stdInput);
-    printf("LLEGUE\n");
-    write(inputFds[1],stdInput,strlen(stdInput));
-    printf("ESCRIBI\n");
+    if(pipe(fds) < 0) { exit(1); }
 
-    if (fork() == 0) {
-        close(inputFds[1]);
-        close(outputFds[0]);
-        dup2(inputFds[0],0);
-        dup2(outputFds[1],1);
-        printf("HICE DUP\n");
-        execl("/bin/sh",command,NULL);
-    } else {
+    if(fork() != 0) {
+        close(fds[1]);
+        dup2(fds[0], 0);
+        int status;
         while(wait(&status) > 0);
-        close(inputFds[0]);
-        close(outputFds[1]);
-        bytesRead = read(outputFds[0],commandOutput,1000);
-        printf("LEI\n");
-        printf("%s\n",commandOutput);
+        scanf("%s", stdInput); // despues de que hijo haya escrito en stdin de padre
+        printf("%s\n", stdInput);
     }
-
+    else {
+        close(fds[0]);
+        dup2(fds[1], 1);
+        execl("/bin/sh", "/bin/sh", "-c", command, NULL);
+    }
 }
