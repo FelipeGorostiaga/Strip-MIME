65a39611dda4853c2b15fb928f9d2c777bb259a1
Santiago Swinnen <sswinnen@itba.edu.ar>
2018-08-25 12:18:28 -0300

    Input working

--- main.c
+++ main.c
@@ -2,21 +2,44 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <string.h>
 #include <sys/wait.h>
 
+typedef void (*slaveFunc)(int * outputFds, char * command);
+
+char * concatArgs(int argc, char * argv[]);
+void createSlave(slaveFunc sFunc, char * command);
+void executeCommand(int * outputFds, char * command);
+void inputParity(int * outputFds, char * command);
+char * parity(char * str);
+
 int main(int argc, char * argv []) {
+
     char * command = NULL;
     char commandOutput[1000] = {0};
-    char stdInput[BLOCK] = {0};
     size_t cmdLength = 0;
-    int i, j;
-    int fds[2] = {-1,-1};
     ssize_t bytesRead;
-    int status;
+    size_t stdInLen = 0;
 
     if(argc <= 1)
         return 0;
+
+    command = concatArgs(argc,argv);
+
+    createSlave(inputParity,NULL);
+    createSlave(executeCommand,command);
+
+    scanf("%s", commandOutput); /*after child writes in parent's stdin*/
+
+    fprintf(stderr,"%s!!!\n",parity(commandOutput));
+    printf("%s\n", commandOutput);
+}
+
+/*Concatenates command line arguments*/
+char * concatArgs(int argc, char * argv[]) {
+
+    int i, j;
+    size_t cmdLength = 0;
+    char * command = NULL;
 
     for(i = 1; i < argc; i++) {
         for(j = 0; argv[i][j] != 0; j++) {
@@ -36,19 +59,50 @@ int main(int argc, char * argv []) {
         exit(1);
     }
 
-    if(pipe(fds) < 0) { exit(1); }
+    return command;
+}
+
+void createSlave(slaveFunc sFunc, char * command) {
+
+    int outputFds[2] = {-1,-1};
+    int status;
+
+    if(pipe(outputFds) < 0) { exit(1); }
 
     if(fork() != 0) {
-        close(fds[1]);
-        dup2(fds[0], 0);
-        int status;
+        close(outputFds[1]); /*closes write end of the pipe*/
+        dup2(outputFds[0], 0);
         while(wait(&status) > 0);
-        scanf("%s", stdInput); // despues de que hijo haya escrito en stdin de padre
-        printf("%s\n", stdInput);
     }
     else {
-        close(fds[0]);
-        dup2(fds[1], 1);
-        execl("/bin/sh", "/bin/sh", "-c", command, NULL);
+        sFunc(outputFds,command);
+        exit(0);
     }
+}
+
+void inputParity(int * outputFds, char * command) {
+    char stdInput[1000];
+
+    close(outputFds[0]);
+    dup2(outputFds[1], 1);
+
+    scanf("%s",stdInput);
+    printf("%s", stdInput);
+    fprintf(stderr,"%s!\n",parity(stdInput));
+}
+
+char * parity(char * str) {
+    int i, j, sum = 0;
+    for(i = 0; str[i] != 0; i++) {
+        for(j = 0; j < 8; j++) {
+            sum += str[i] & (1 << j);
+        }
+    }
+    
+}
+
+void executeCommand(int * outputFds, char * command) {
+    close(outputFds[0]);
+    dup2(outputFds[1], 1);
+    execl("/bin/sh", "/bin/sh", "-c", command, NULL);
 }
